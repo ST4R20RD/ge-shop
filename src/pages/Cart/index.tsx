d6b6cc3d17f9d9ba31b8c20b223a07f90e43bfe7
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-10-03 15:21:43 +0100

    Cart "total" Section stay even when empty and changed bg color

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -11,7 +11,7 @@ import styled from "styled-components";
 export function Cart() {
   const [allProducts, getAllProducts, productsFetchState] = useGetAllProducts();
   const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext) as ShopContextType;
-  const totalAmount = getTotalCartAmount();
+  const totalAmount = getTotalCartAmount() || 0;
 
   const navigate = useNavigate();
 
@@ -23,10 +23,10 @@ export function Cart() {
   return (
     <div className="flex flex-col items-center max-w-screen-xl h-[calc(100vh-64px)]">
       <h1>Cart</h1>
-      {totalAmount && totalAmount > 0 ? (
-        <section className="flex justify-center">
-          <ProductList className="flex flex-col items-end overflow-y-scroll w-[42vw]">
-            {productsFetchState === FetchState.SUCCESS && (
+      <section className="flex justify-center">
+        <ProductList className="flex flex-col items-end overflow-y-auto w-[42vw]">
+          {totalAmount && totalAmount > 0 ? (
+            productsFetchState === FetchState.SUCCESS && (
               <>
                 {allProducts
                   .filter((product) => cartItems[product.id] > 0)
@@ -34,52 +34,50 @@ export function Cart() {
                     return <InCartProductBox product={product} />;
                   })}
               </>
-            )}
-          </ProductList>
-          <div className="mt-2">
-            <>
-              <div className="text-xl font-semibold rounded-lg bg-white p-5 w-80 mb-4">
-                <h1> Total: </h1>
-                <h2 className="my-5 text-right">${totalAmount.toFixed(2)}</h2>
-                <div className="flex flex-col">
-                  <button
-                    className="rounded-md border p-5 py-3 my-2 text-red-600 text-xl bg-red-100"
-                    onClick={() => navigate("/")}
-                  >
-                    Continue Shopping
-                  </button>
-                  <button
-                    className="rounded-md border px-5 py-3 my-2 text-white text-xl bg-red-600"
-                    onClick={() => {
-                      checkout();
-                      navigate("/checkout");
-                    }}
-                  >
-                    Checkout
-                  </button>
-                </div>
-              </div>
-              <div className="text-xl font-semibold rounded-lg bg-white p-5 w-80 my-4">
-                <p>Pagar com</p>
-                <Payments className="flex text-4xl my-5">
-                  <LiaCcVisa />
-                  <FaCcMastercard />
-                  <FaPaypal />
-                  <FaGooglePay />
-                  <FaApplePay />
-                </Payments>
-                <div className="flex text-sm font-semibold ">
-                  <FaShieldAlt color="#007B61" className="absolute mt-1" size={15} />
-                  &nbsp;&nbsp;&nbsp;&nbsp; Proteção ao cliente Recupere o seu reembolso se o artigo
-                  não for entregue ou de acordo com a descrição.
-                </div>
-              </div>
-            </>
+            )
+          ) : (
+            <h1 className="mr-5"> Your Shopping Cart is Empty</h1>
+          )}
+        </ProductList>
+        <div className="mt-2">
+          <div className="text-xl font-semibold rounded-lg bg-white p-5 w-80 mb-4">
+            <h1> Total: </h1>
+            <h2 className="my-5 text-right">${totalAmount.toFixed(2)}</h2>
+            <div className="flex flex-col">
+              <button
+                className="rounded-md border p-5 py-3 my-2 text-red-600 text-xl bg-red-100"
+                onClick={() => navigate("/")}
+              >
+                Continue Shopping
+              </button>
+              <button
+                className="rounded-md border px-5 py-3 my-2 text-white text-xl bg-red-600"
+                onClick={() => {
+                  checkout();
+                  navigate("/checkout");
+                }}
+              >
+                Checkout
+              </button>
+            </div>
           </div>
-        </section>
-      ) : (
-        <h1> Your Shopping Cart is Empty</h1>
-      )}
+          <div className="text-xl font-semibold rounded-lg bg-white p-5 w-80 my-4">
+            <p>Pagar com</p>
+            <Payments className="flex text-4xl my-5">
+              <LiaCcVisa />
+              <FaCcMastercard />
+              <FaPaypal />
+              <FaGooglePay />
+              <FaApplePay />
+            </Payments>
+            <div className="flex text-sm font-semibold ">
+              <FaShieldAlt color="#007B61" className="absolute mt-1" size={15} />
+              &nbsp;&nbsp;&nbsp;&nbsp; Proteção ao cliente Recupere o seu reembolso se o artigo não
+              for entregue ou de acordo com a descrição.
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }

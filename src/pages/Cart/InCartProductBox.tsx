ea486798c8619f128e871ac63b8ff53b99bb97ce
ST4R20RD <goncalo.estrelado@hotmail.com>
2023-10-03 02:12:09 +0100

    first iteration of Cart Design

---
+++ src/pages/Cart/InCartProductBox.tsx
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+import { ProductData } from "../../utils/types";
+import { useEffect, useRef, useContext } from "react";
+import { ShopContext, ShopContextType } from "../../context/ShopContext";
+import { Link } from "react-router-dom";
+import { FaMinus, FaPlus } from "react-icons/fa";
+
+interface Props {
+  product: ProductData;
+}
+
+export function InCartProductBox({ product }: Props) {
+  const { addToCart, removeFromCart, cartItems } = useContext(ShopContext) as ShopContextType;
+  const priceInteger = Math.trunc(product.price);
+  const priceDecimal = ((product.price - priceInteger) * 100).toFixed();
+
+  const discount = 50;
+
+  const cartItemCount = cartItems[product.id];
+
+  const cartItemCountRef = useRef<any>();
+  useEffect(() => {
+    const cartItemCountValue = cartItems[product.id];
+    cartItemCountRef.current = cartItemCountValue;
+    // eslint-disable-next-line
+  }, [cartItems]);
+
+  return (
+    <div className="bg-white shadow-sm rounded-xl w-9/12 m-2">
+      <Wrapper>
+        <Container className="p-2">
+          <div className="flex">
+            <Link to={`/product/${product.id}`}>
+              <div className="flex justify-center items-center h-36 w-36 mr-8">
+                <Img src={product.image} className="max-h-36" />
+              </div>
+            </Link>
+            <div>
+              <Title className="text-black font-semibold text-sm h-10">{product.title}</Title>
+              <p className="text-white bg-red-500 w-fit rounded-tr-xl rounded-br-xl px-2">
+                -{discount}%
+              </p>
+              <div>
+                <Price className="flex items-end text-red-600 text-lg mb-2">
+                  <p className="text-3xl">${priceInteger}</p>
+                  {product.price - priceInteger > 0 && "," + priceDecimal}
+                  <del className="text-zinc-500 ml-2">
+                    ${(product.price * (1 + discount / 100)).toFixed(2)}
+                  </del>
+                </Price>
+              </div>
+              <p className="text-green-600 font-semibold">Delivery in 10 days</p>
+            </div>
+          </div>
+          <div className="flex items-center mx-5">
+            <Button
+              className="p-1 rounded-full"
+              onClick={() => {
+                removeFromCart(product.id);
+              }}
+            >
+              <FaMinus />
+            </Button>
+            <input
+              defaultValue={1}
+              value={cartItemCount}
+              className="w-10 h-6 text-center border border-zinc-600"
+            />
+            <Button
+              className="p-1 rounded-full"
+              onClick={() => {
+                addToCart(product.id);
+              }}
+            >
+              <FaPlus />
+            </Button>
+          </div>
+        </Container>
+      </Wrapper>
+    </div>
+  );
+}
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  /* height: calc(100% - 24px); */
+`;
+const Img = styled.img``;
+const Title = styled.p``;
+const Price = styled.p``;
+const Button = styled.button`
+  color: red;
+  &:hover {
+    color: #ffffff;
+    background-color: red;
+  }
+`;
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,3 +1,94 @@
+import { useContext, useEffect } from "react";
+import { useGetAllProducts } from "../../lib/api-hooks";
+import { ShopContext, ShopContextType } from "../../context/ShopContext";
+import { InCartProductBox } from "./InCartProductBox";
+import { FetchState } from "../../utils/types";
+import { useNavigate } from "react-router-dom";
+import { FaApplePay, FaCcMastercard, FaGooglePay, FaPaypal, FaShieldAlt } from "react-icons/fa";
+import { LiaCcVisa } from "react-icons/lia";
+import styled from "styled-components";
+
 export function Cart() {
-  return <div>Cart</div>;
+  const [allProducts, getAllProducts, productsFetchState] = useGetAllProducts();
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext) as ShopContextType;
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getAllProducts();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center max-w-screen-xl h-[calc(100vh-64px)]">
+      <h1>Cart</h1>
+      {totalAmount && totalAmount > 0 ? (
+        <section className="flex justify-center">
+          <ProductList className="flex flex-col items-end overflow-y-scroll w-[42vw]">
+            {productsFetchState === FetchState.SUCCESS && (
+              <>
+                {allProducts
+                  .filter((product) => cartItems[product.id] > 0)
+                  .map((product) => {
+                    return <InCartProductBox product={product} />;
+                  })}
+              </>
+            )}
+          </ProductList>
+          <div className="mt-2">
+            <>
+              <div className="text-xl font-semibold rounded-lg bg-white p-5 w-80 mb-4">
+                <h1> Total: </h1>
+                <h2 className="my-5 text-right">${totalAmount.toFixed(2)}</h2>
+                <div className="flex flex-col">
+                  <button
+                    className="rounded-md border p-5 py-3 my-2 text-red-600 text-xl bg-red-100"
+                    onClick={() => navigate("/")}
+                  >
+                    Continue Shopping
+                  </button>
+                  <button
+                    className="rounded-md border px-5 py-3 my-2 text-white text-xl bg-red-600"
+                    onClick={() => {
+                      checkout();
+                      navigate("/checkout");
+                    }}
+                  >
+                    Checkout
+                  </button>
+                </div>
+              </div>
+              <div className="text-xl font-semibold rounded-lg bg-white p-5 w-80 my-4">
+                <p>Pagar com</p>
+                <Payments className="flex text-4xl my-5">
+                  <LiaCcVisa />
+                  <FaCcMastercard />
+                  <FaPaypal />
+                  <FaGooglePay />
+                  <FaApplePay />
+                </Payments>
+                <div className="flex text-sm font-semibold ">
+                  <FaShieldAlt color="#007B61" className="absolute mt-1" size={15} />
+                  &nbsp;&nbsp;&nbsp;&nbsp; Proteção ao cliente Recupere o seu reembolso se o artigo
+                  não for entregue ou de acordo com a descrição.
+                </div>
+              </div>
+            </>
+          </div>
+        </section>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
+    </div>
+  );
 }
+
+const Payments = styled.div`
+  svg {
+    margin: 0 8px;
+  }
+`;
+const ProductList = styled.div`
+  height: calc(100vh - 112px);
+`;
